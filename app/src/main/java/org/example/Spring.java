03ec836aa50a17599d250ee0ffd0319a29cd0b35
Alex <X00197973@mytudublin.ie>
2025-12-13 15:47:10 +0000

    More work

---
+++ app/src/main/java/org/example/Spring.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring {
+
+	public static void main(String[] args) throws Throwable {
+		SpringApplication.run(Spring.class, args);
+	}
+
+}
